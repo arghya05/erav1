d91e5ba723f982dc354b31f222510a7c7f168099
shashankhalo7 <shashankhalo7@gmail.com>
2020-10-01 19:03:53 +0530

    lr_finder

--- Assignment 10/src/lr_finder.py
+++ Assignment 10/src/lr_finder.py
@@ -18,7 +18,86 @@ try:
 except ImportError:
     IS_AMP_AVAILABLE = False
 
+class DataLoaderIter(object):
+    def __init__(self, data_loader):
+        self.data_loader = data_loader
+        self._iterator = iter(data_loader)
 
+    @property
+    def dataset(self):
+        return self.data_loader.dataset
+
+    def inputs_labels_from_batch(self, batch_data):
+        if not isinstance(batch_data, list) and not isinstance(batch_data, tuple):
+            raise ValueError(
+                "Your batch type is not supported: {}. Please inherit from "
+                "`TrainDataLoaderIter` or `ValDataLoaderIter` and override the "
+                "`inputs_labels_from_batch` method.".format(type(batch_data))
+            )
+
+        inputs, labels, *_ = batch_data
+
+        return inputs, labels
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        batch = next(self._iterator)
+        return self.inputs_labels_from_batch(batch)
+
+
+class TrainDataLoaderIter(DataLoaderIter):
+    def __init__(self, data_loader, auto_reset=True):
+        super().__init__(data_loader)
+        self.auto_reset = auto_reset
+
+    def __next__(self):
+        try:
+            batch = next(self._iterator)
+            inputs, labels = self.inputs_labels_from_batch(batch)
+        except StopIteration:
+            if not self.auto_reset:
+                raise
+            self._iterator = iter(self.data_loader)
+            batch = next(self._iterator)
+            inputs, labels = self.inputs_labels_from_batch(batch)
+
+        return inputs, labels
+
+
+class ValDataLoaderIter(DataLoaderIter):
+    """This iterator will reset itself **only** when it is acquired by
+    the syntax of normal `iterator`. That is, this iterator just works
+    like a `torch.data.DataLoader`. If you want to restart it, you
+    should use it like:
+        ```
+        loader_iter = ValDataLoaderIter(data_loader)
+        for batch in loader_iter:
+            ...
+        # `loader_iter` should run out of values now, you can restart it by:
+        # 1. the way we use a `torch.data.DataLoader`
+        for batch in loader_iter:        # __iter__ is called implicitly
+            ...
+        # 2. passing it into `iter()` manually
+        loader_iter = iter(loader_iter)  # __iter__ is called by `iter()`
+        ```
+    """
+
+    def __init__(self, data_loader):
+        super().__init__(data_loader)
+        self.run_limit = len(self.data_loader)
+        self.run_counter = 0
+
+    def __iter__(self):
+        if self.run_counter >= self.run_limit:
+            self._iterator = iter(self.data_loader)
+            self.run_counter = 0
+        return self
+
+    def __next__(self):
+        self.run_counter += 1
+        return super(ValDataLoaderIter, self).__next__()
 
 class LRFinder(object):
     """Learning rate range test.
